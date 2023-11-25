0f7a206d5ee25f93ae3f72ee0b03711cd759c52c
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2023-11-25 22:32:21 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,11 @@
+function solution(a, b, c) {
+  if (a === b && b === c && c === a) {
+    return (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c))
+  } else {
+    if (a === b || b === c || c === a) {
+      return (a + b + c) * ((a * a) + (b * b) + (c * c))
+    } else {
+      return a + b + c
+    }
+  }
+}
