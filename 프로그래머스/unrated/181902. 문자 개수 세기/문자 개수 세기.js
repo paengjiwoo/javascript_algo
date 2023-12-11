28de60d7e513919c0b0c43f75eefe9c5ae3adb9b
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2023-12-11 22:06:01 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+  let ans = Array.from({length : 52}, () => 0)
+  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  for (let i = 0; i < my_string.length; i++) {
+    ans[alphabet.indexOf(my_string[i])] += 1
+  }
+  return ans;
+}
