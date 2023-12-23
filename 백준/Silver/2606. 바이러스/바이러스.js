3693423045ce550596a458ca59c9a32117ec138e
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2023-12-23 20:14:16 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 9412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -2,24 +2,30 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const N = parseInt(input[0])
 const M = parseInt(input[1])
 
-let graph = Array.from({length:N+1}, () => Array.from({length:N+1}, () => false))
+let graph = Array.from({length:N+1}, () => [])
 for (let i = 2; i < M + 2; i++) {
   const [a, b] = input[i].split(' ').map(Number)
-  graph[a][b] = true
-  graph[b][a] = true
+  graph[a].push(b)
+  graph[b].push(a)
 }
 
-let visited = Array.from({length:N+1}, () => false)
+let visited = Array.from({length:N + 1}, () => false)
 let ans = 0
-function dfs(n) {
+
+function bfs(n) {
+  let q = [n]
   visited[n] = true
-  for (let i = 1; i <= N; i++) {
-    if (!visited[i] && graph[n][i]) {
-      ans++;
-      dfs(i)
+  while (q.length) {
+    let node = q.shift()
+    for (let i = 1; i <= N; i++) {
+      if(!visited[i] && graph[node].includes(i)) {
+        visited[i] = true;
+        ans++;
+        q.push(i)
+      }
     }
   }
 }
 
-dfs(1)
+bfs(1)
 console.log(ans)
