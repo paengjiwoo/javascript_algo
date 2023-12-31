7b0f6427e660806d49a5036f4a3fc9b6a3d6fdc3
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2023-12-31 21:05:26 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.js
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.js
@@ -1,14 +1,9 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
-const [N, M, K] = input.map(num => Number(num))
-
+const [N, M, K] = input.map(Number)
 let max = 0
 for (let i = 0; i <= K; i++) {
   const girl = Math.floor((N - i) / 2)
   const boy = M - (K - i)
-  const min = Math.min(girl, boy)
-  if (max < min) {
-    max = min
-  }
+  max = Math.max(max, Math.min(girl, boy))
 }
-
 console.log(max)
