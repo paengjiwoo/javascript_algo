6c7fa01a6bae9ac60130825b0cd7fed3a2e75e26
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2023-12-31 20:12:19 +0900

    [Bronze III] Title: 완전 제곱수, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.js
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.js
@@ -2,7 +2,7 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim()
 const N = parseInt(input)
 let ans = 0
 
-for (let b = 1; b <= 500; b++) {
+for (let b = 1; b <= N; b++) {
   if (Number.isInteger(Math.sqrt(Math.pow(b, 2) + N))) {
     ans++
   }
