c83738cb28afb1b5b51678407cfa29e4453f9417
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2024-01-07 13:00:36 +0900

    [Gold V] Title: 공약수, Time: 124 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.js
+++ 백준/Gold/2436. 공약수/공약수.js
@@ -1,18 +1,21 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
 const [a, b] = input.map(Number)
 
-function GCD(x, y) {
-  if (!(x % y)) {
-    return y
+function GCD(x, y, res) {
+  while (y > 0) {
+    res = x % y 
+    x = y
+    y = res
   }
-  return GCD(y, x % y)
+  return x
 }
 
 for (let i = Math.floor(Math.sqrt(b / a)); i > 0; i--) {
   if (((b / a) % i)) continue
   const j = (b / a) / i
   if (i > j) break
-  if (GCD(i, j) === 1) {
+  let res = 0
+  if (GCD(i, j, res) === 1) {
     console.log(i * a, j * a)
     break
   }
