5a35711201008274329be9d8a4fb3d1ab4914d46
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2024-01-31 16:22:00 +0900

    [Silver III] Title: N과 M (3), Time: 836 ms, Memory: 149280 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.js
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -2,24 +2,20 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 const [N, M] = input.split(' ').map(Number)
 
 let res = []
-function permutation(ans, cnt, checked) {
+function permutation(ans, cnt) {
   if (cnt === M) {
     res.push(ans.join(' '))
     return
   }
   
   for (let i = 1; i < N + 1; i++) {
-    checked[i] = 1
     ans.push(i)
     cnt++;
-    permutation(ans, cnt, checked)
+    permutation(ans, cnt)
     cnt--;
     ans.pop()
-    checked[i] = 0
   }
 }
 
-let arr = Array.from({length: N + 1}, () => 0)
-permutation([], 0, arr)
-
+permutation([], 0)
 console.log(res.join('\n'))
