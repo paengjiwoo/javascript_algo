c80430590a71a006aec7f26f9c0e2200b45f238d
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2024-02-08 00:20:42 +0900

    [Silver I] Title: 숨바꼭질, Time: 448 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,21 +1,27 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 const [N, K] = input.split(' ').map(Number)
 
-let visited = Array.from({length: 100001}, () => -1)
-
-let ans = -1
-let q, temp
-visited[N] = 0
-q = [N]
-while (ans < 0) {
-  temp = q.shift()
-  if (temp === K) ans = visited[temp]
-
-  for (const num of [temp - 1, temp + 1, temp * 2]) {
-    if (0 <= num && num <= 100000 && visited[num] < 0) {
-      visited[num] = visited[temp] + 1
-      q.push(num)
+if (N === K) {
+  console.log(0)
+} else {
+  let visited = Array.from({length: 100001}, () => -1)
+  let ans = -1
+  let q, temp
+  visited[N] = 0
+  q = [N]
+  while (ans < 0) {
+    temp = q.shift()
+  
+    for (const num of [temp - 1, temp + 1, temp * 2]) {
+      if (0 <= num && num <= 100000 && visited[num] < 0) {
+        if (num === K) {
+          ans = visited[temp] + 1
+          break
+        }
+        visited[num] = visited[temp] + 1
+        q.push(num)
+      }
     }
   }
+  console.log(ans)
 }
-console.log(ans)
