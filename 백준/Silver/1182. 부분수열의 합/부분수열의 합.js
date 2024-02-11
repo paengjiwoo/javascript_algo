01ffa4472622fb678feebed485abee6419c9cc01
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2024-02-11 11:38:15 +0900

    [Silver II] Title: 부분수열의 합, Time: 188 ms, Memory: 9988 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -1,15 +1,15 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const [N, S] = input.shift().split(' ').map(Number)
 const nums = input[0].split(' ').map(Number)
-
 let cnt = 0
-function solve(sum, idx) {
-  if (sum === S && idx) { 
-    cnt++;
+
+function solve(idx, sum) {
+  if (sum === S && idx) {
+    cnt++
   }
 
   for (let i = idx; i < N; i++) {
-    solve(sum + nums[i], i + 1)
+    solve(i + 1, sum + nums[i])
   }
 }
 solve(0, 0)
