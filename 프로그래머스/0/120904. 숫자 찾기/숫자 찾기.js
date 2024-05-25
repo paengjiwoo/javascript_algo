a8d3e1fd6ff371508755228e234283a7788ca1e3
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2024-05-25 14:53:41 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -0,0 +1,9 @@
+function solution(num, k) {
+  const str = num.toString();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === k.toString()) {
+      return i + 1;
+    }
+  }
+    return -1;
+}
