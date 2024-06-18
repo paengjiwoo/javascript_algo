5902e764bae03b254cb84f971038ac251dbe1371
paeng <122685653+JiwooPaeng@users.noreply.github.com>
2024-06-18 15:58:14 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -3,24 +3,16 @@ function solution(keyinput, board) {
   for (let i = 0; i < keyinput.length; i++) {
     switch (keyinput[i]) {
       case "left":
-          if (Math.abs(point[0] - 1) < board[0] / 2) {
-              point[0] -= 1;
-          }
+          if (-(point[0] - 1) < board[0] / 2) point[0]--;
           break;
       case "right":
-          if (point[0] + 1 < board[0] / 2) {
-              point[0] += 1;
-          }
+          if (point[0] + 1 < board[0] / 2) point[0]++;
           break;
       case "up":
-          if (point[1] + 1 < board[1] / 2) {
-              point[1] += 1;
-          }
+          if (point[1] + 1 < board[1] / 2) point[1]++;
           break;
       case "down":
-          if (Math.abs(point[1] - 1) < board[1] / 2) {
-              point[1] -= 1;
-          }
+          if (-(point[1] - 1) < board[1] / 2) point[1]--;
           break;
     }
   }
