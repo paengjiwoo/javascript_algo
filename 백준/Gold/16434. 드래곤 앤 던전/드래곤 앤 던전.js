24c2ecc10983fa584701ccdf9ff0bc666883d29d
팽지우 <paengzw@gmail.com>
2024-09-10 13:46:54 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 1708 ms, Memory: 65568 KB -BaekjoonHub

---
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.js
@@ -0,0 +1,45 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input/16434.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+const [len, initialAtk] = input.shift().split(' ').map(Number);
+const stages = input.map(stage => stage.split(' ').map(Number));
+
+function checkATK(maxHP) {
+  let playerHP = maxHP;
+  let playerATK = initialAtk;
+  for (let [type, atk, hp] of stages) {
+    if (type === 1) {
+      const attackTurns = Math.ceil(hp / playerATK);
+      const damageTurns = attackTurns - 1;
+      const totalDamage = BigInt(damageTurns * atk);
+
+      if (playerHP <= totalDamage) return false;
+
+      playerHP -= totalDamage;
+
+    } else if (type === 2) {
+      playerHP = maxHP > playerHP + BigInt(hp) ? playerHP + BigInt(hp) : maxHP;
+      playerATK += atk;
+    }
+  }
+  
+  return playerHP > 0;
+};
+
+let start = 1n;
+let end = BigInt(len) * 1000000n * 1000000n;
+let mid;
+let ans;
+
+while (start < end) {
+  mid = (start + end) / 2n;
+
+  if (checkATK(mid)) {
+    ans = mid;
+    end = mid;
+  } else {
+    start = mid + 1n;
+  }
+}
+
+console.log(ans.toString());
