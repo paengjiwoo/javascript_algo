511624afa00fde7061bcd46f454542f79cc19e29
팽지우 <paengzw@gmail.com>
2024-09-12 13:44:26 +0900

    [Gold IV] Title: 고냥이, Time: 156 ms, Memory: 10240 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.js
+++ 백준/Gold/16472. 고냥이/고냥이.js
@@ -1,37 +1,37 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const N = Number(input.shift())
-const str = input[0]
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-if (str.length === 1) {
-  console.log(1)
-} else {
-  let memo = {}
-  for (let i = 0; i < 26; i++) {
-    memo[String.fromCharCode(i + 97)] = 0
-  }
+const diff = Number(input.shift());
+const meow = input[0];
 
-  let s = 0
-  let e = 1
-  let cnt = (str[0] === str[1] ? 1 : 2)
-  memo[str[0]] += 1
-  memo[str[1]] += 1
-  let ans = 1
-  while (s < str.length - ans && e < str.length - 1) {
-    if (cnt <= N) {
-      e++;
-      if (!memo[str[e]]) cnt++;
-      memo[str[e]] += 1
-    } else {
-      memo[str[s]] -= 1
-      if (!memo[str[s]]) cnt--;
-      s++;
-      if (e - s + 1 < ans) {
-        e++;
-        if (!memo[str[e]]) cnt++;
-        memo[str[e]] += 1
-      }
+let left = 0;
+let right = 0;
+let ans = 0;
+
+// 알파벳 소문자의 아스키 코드는 97에서 123, 총 26개 알파벳
+let alphabet = new Array(26).fill(0);
+let cnt = 0;
+
+while (right < meow.length) {
+  let rightMeow = meow[right].codePointAt();
+  if (!alphabet[rightMeow - 97]) {
+    cnt++;
+  };
+  alphabet[rightMeow - 97]++;
+  right++;
+
+ 
+  while (diff < cnt && left < right) {
+    let leftMeow = meow[left].codePointAt();
+    alphabet[leftMeow - 97]--;
+    left++;
+
+    if (!alphabet[leftMeow - 97]) {
+      cnt--;
     }
-    if (cnt <= N && ans < e - s + 1) ans = e - s + 1
   }
-  console.log(ans)
+
+  ans = Math.max(ans, right - left);
 }
+
+console.log(ans);
