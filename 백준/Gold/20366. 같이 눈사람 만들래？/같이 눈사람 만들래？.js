3a7962cad9e542cc5ed8c327dcd6af6a73d1106b
팽지우 <paengzw@gmail.com>
2024-09-24 22:21:42 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 368 ms, Memory: 9936 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.js
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.js
@@ -1,4 +1,5 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const fs = require("fs");
+const input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt").toString().trim().split("\n");
 const N = parseInt(input.shift())
 const snowballs = input[0].split(' ').map(Number).sort((a, b) => a - b)
 
